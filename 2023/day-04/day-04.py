6ed9cb33670fd1e9e49d1fd3de6f7bd3f621aeed
Simon Stone <simon.stone@dartmouth.edu>
2023-12-04 20:05:19 -0500

    day 4 part 2

--- 2023/day-04/day-04.py
+++ 2023/day-04/day-04.py
@@ -1,16 +1,29 @@
+def parse_card(card):
+    return map(
+        lambda x: set(map(int, x.split())), card.strip().split(":")[1].split("|")
+    )
+
+
 def part_01(cards):
     score = 0
     for card in cards:
-        winners, my_numbers = map(
-            lambda x: set(map(int, x.split())), card.strip().split(":")[1].split("|")
-        )
+        winners, my_numbers = parse_card(card)
         if n_winners := len(winners.intersection(my_numbers)):
             score += 2 ** (n_winners - 1)
     return score
 
 
-def part_02(x):
-
+def part_02(cards):
+    n_cards = []
+    n_winners = []
+    for card in cards:
+        n_cards.append(1)
+        winners, my_numbers = parse_card(card)
+        n_winners.append(len(winners.intersection(my_numbers)))
+    for card_idx in range(len(n_cards)):
+        for offset in range(1, n_winners[card_idx] + 1):
+            n_cards[card_idx + offset] += n_cards[card_idx]
+    return sum(n_cards)
 
 
 if __name__ == "__main__":
@@ -18,4 +31,4 @@ if __name__ == "__main__":
         cards = f.readlines()
 
     print(part_01(cards))
-    print(part_02())
+    print(part_02(cards))

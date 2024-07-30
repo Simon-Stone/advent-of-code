676e1e9af2fecab3e0553047ea5dd30195fc6c9d
Simon Stone <simon.stone@dartmouth.edu>
2024-07-29 20:04:11 -0400

    tricky 13

--- 2023/day-13/day-13.py
+++ 2023/day-13/day-13.py
@@ -12,11 +12,12 @@ def check_symmetry(matrix, top, bottom):
     return True
 
 
-def find_axis(matrix):
+def find_axis(matrix, old_axis=None):
     for idx, (a, b) in enumerate(pairwise(matrix)):
         if np.all(a == b):
             if check_symmetry(matrix, idx, idx + 1):
-                return idx
+                if old_axis is None or old_axis != idx:
+                    return idx
     return None
 
 
@@ -42,17 +43,26 @@ def part_02(patterns):
         m = pattern_to_matrix(pattern)
         orig_line_row = find_axis(m)
         orig_line_col = find_axis(m.T)
+        n_hits = 0
         for idx in np.ndindex(*m.shape):
             m_fixed = m.copy()
             m_fixed[idx] = "#" if m[idx] == "." else "."
-            if (fixed_line_row := find_axis(m_fixed)) is not None:
+            if (
+                fixed_line_row := find_axis(m_fixed, old_axis=orig_line_row)
+            ) is not None:
                 if orig_line_row != fixed_line_row:
                     above_line += fixed_line_row + 1
+                    n_hits += 1
                     break
-            if (fixed_line_col := find_axis(m_fixed.T)) is not None:
+            if (
+                fixed_line_col := find_axis(m_fixed.T, old_axis=orig_line_col)
+            ) is not None:
                 if orig_line_col != fixed_line_col:
                     left_of_line += fixed_line_col + 1
+                    n_hits += 1
                     break
+        if n_hits == 0:
+            print(m)
     return left_of_line + 100 * above_line
 
 
@@ -63,6 +73,6 @@ def pattern_to_matrix(pattern):
 
 if __name__ == "__main__":
     with open("./2023/day-13/input.txt") as f:
-        patterns = f.read().split("\n\n")
+        patterns = f.read().strip().split("\n\n")
     print(part_01(patterns))
     print(part_02(patterns))
